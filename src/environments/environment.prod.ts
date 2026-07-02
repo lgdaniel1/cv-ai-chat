5c8e99d18cf7c971f26f77e52bce284acc5628fb
zetesdaniel <79923062+zetesdaniel@users.noreply.github.com>
2026-07-02 20:09:13 +0200

    change backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://cv-ai-chat-backend-9700.azurewebsites.net',
+  backendUrl:
+    'https://cv-ai-chat-backend-api-addbb4eyb6gmg3bp.westeurope-01.azurewebsites.net',
 };
